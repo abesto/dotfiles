7318eb234df187ac5f998b31d098109fc22e5d22
Nagy Zoltán <abesto0@gmail.com>
2009-02-08 11:19:24 +0100

    Put back haskell unconditional load

--- .emacs.d/my-haskell-init.el
+++ .emacs.d/my-haskell-init.el
@@ -1,4 +1,3 @@
-(defun load-haskell ()
-  (load (expand-file-name "~/.emacs.d/site/haskell-mode-2.4/haskell-site-file"))
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent))
+(load (expand-file-name "~/.emacs.d/site/haskell-mode-2.4/haskell-site-file"))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
