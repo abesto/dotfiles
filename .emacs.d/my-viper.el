404e3d2b85e608534a26914c4f765821d4339d7c
Nagy Zoltán <abesto0@gmail.com>
2009-02-21 11:55:28 +0100

    Unbind öüóéáűí

--- .emacs.d/my-viper.el
+++ .emacs.d/my-viper.el
@@ -12,3 +12,14 @@
 (require 'rect-mark)               ; nice block mode
 (setq scroll-conservatively 0)     ; vim-style scrolling
 (setq scroll-margin 5)             ; and again
+
+;; Unbind accented chars
+(define-key viper-vi-basic-map "ö" 'viper-nil)
+(define-key viper-vi-basic-map "ü" 'viper-nil)
+(define-key viper-vi-basic-map "ó" 'viper-nil)
+(define-key viper-vi-basic-map "ő" 'viper-nil)
+(define-key viper-vi-basic-map "ú" 'viper-nil)
+(define-key viper-vi-basic-map "é" 'viper-nil)
+(define-key viper-vi-basic-map "á" 'viper-nil)
+(define-key viper-vi-basic-map "ű" 'viper-nil)
+(define-key viper-vi-basic-map "í" 'viper-nil)
