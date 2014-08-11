c157b6ff75633797a3e8ad9ac06b0f0356b7a632
Zoltan Nagy <abesto@abesto.net>
2014-08-11 11:30:17 +0200

    Smarter SSH functions

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -13,6 +13,7 @@ alias gk='gitk'
 alias grh='git reset --hard'
 alias glg="git log --graph --pretty=format:'%Cred%H%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gri="git rebase --interactive"
+alias gup="git up"
 
 # Shortcuts for activating and deactivating virtualenvs
 v() {
@@ -41,7 +42,11 @@ alias rmtt='TEST_WITH_REMOTE=1 mtt'
 
 alias irc-tunnel='ssh -f abesto@direct.abesto.net -L 6667:direct.abesto.net:6667 -N'
 
-# from https://github.com/prezi/dotfiles/blob/zsol/ec2/aliases.zsh
+CHEF_HOME=${CHEF_HOME:-$HOME/.prezi/prezi-chef}
+find_roles() {
+    regex="$1"; shift
+    ls $CHEF_HOME/roles | grep "$regex" | sed 's/\.json$//'
+}
 alias instances="ec2-describe-instances"
 essh() {
     id=$(echo $1 | grep -o 'i-[0-9a-f]\{8\}')
@@ -50,25 +55,38 @@ essh() {
 }
 ec2_ssh() {
     id=$1; shift
+    echo "Logging in to EC2 instance $id"
     ec2host=$(ec2-describe-instances --show-empty-fields $id | grep '^INSTANCE' | cut -f4)
     ssh $ec2host "$@"
 }
 chef_ssh() {
-    role=$1; shift
-    chefhost=$(knife search node role:$role -R 1 -Fj | jq -r '.rows[0].automatic.cloud.public_hostname')
-    ssh $chefhost "$@"
+    role_regex=$1; shift
+    role=$(find_roles "$role_regex")
+    if [ $(echo "$role" | wc -w) = '0' ]; then
+        echo "No matching Chef role found"
+    elif [ $(echo "$role" | wc -w) != '1' ]; then
+        echo "Found more than one matching Chef role:"
+        echo "$role"
+    else
+        echo "Logging in to nodes with Chef role $role"
+        cd $CHEF_HOME
+        knife ssh roles:$role csshx -x root
+    fi
 }
 s() {
     id=$(echo $1 | grep -o 'i-[0-9a-f]\{8\}')
     if [ $? -eq 0 ]; then
         shift
+        echo "First argument looks like an EC2 instance id"
         ec2_ssh $id "$@"
-    else
+    elif timeout 0.1 host "$1" > /dev/null; then
+        echo "First argument doesn't look like an EC2 instance id, but I can resolve it as a hostname. Logging in directly."
         ssh "$@"
+    else
+        echo "First argument doesn't look like an EC2 instance id, I can't resolve it as a hostname, assuming it's a Chef role (maybe a regex)"
+        chef_ssh "$@"
     fi
 }
-# eof stolen from zsol
-
 sr() {
 	s "$@" -l root
 }
