d33e9308905faef769969dbc2775a0cee0e797d9
Zoltan Nagy <abesto@abesto.net>
2014-12-02 21:46:40 +0100

    Fish: use more plugins :)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ set fish_theme agnoster
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins autojump brew ec2 extract rvm
+set fish_plugins brew ec2 extract rvm bak better-alias gi localhost node python
 
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom $HOME/.prezi/dotfiles/.oh-my-fish/custom
