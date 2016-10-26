fa916b29eb0c3c52fffdf6b3541f01e16e39047f
Zoltan Nagy <abesto@abesto.net>
2016-10-26 21:05:05 +0200

    [fish] More robust checks for PATH setting

--- fish-abesto/conf.d/go.fish
+++ fish-abesto/conf.d/go.fish
@@ -1,2 +1,2 @@
-set -x GOPATH ~/playground/go
-set -x PATH $PATH /usr/local/opt/go/libexec/bin $GOPATH/bin
+test -d ~/playground/go; and set -x GOPATH ~/playground/go
+test -d /usr/local/opt/go/libexec/bin; and set -x PATH $PATH /usr/local/opt/go/libexec/bin $GOPATH/bin
--- fish-abesto/conf.d/path.fish
+++ fish-abesto/conf.d/path.fish
@@ -1,10 +1,12 @@
 set PATH $HOME/bin $PATH                                               # Mine, all mine!
 set PATH $PATH $HOME/.rvm/bin                                          # language-specific stuff
-set PATH $PATH /usr/X11/bin                                            # X11
+test -d /usr/X11/bin; and set PATH $PATH /usr/X11/bin                  # X11
 set PATH $PATH /sbin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin  # System-wide binaries
-set PATH $PATH $HOME/.prezi/simply
+test -d ~/.prezi/simply; and set PATH $PATH $HOME/.prezi/simply
 
 # GNU Tools
-set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-set PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH
+if test -d /usr/local/opt/coreutils; then
+    set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+    set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+end
+test -d /usr/local/opt/gnu-tar; and set PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH
