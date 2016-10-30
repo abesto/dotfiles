a17477cc5687324bdcb26c22a22662c1a9dbbd73
Zoltan Nagy <abesto@abesto.net>
2016-10-30 15:49:41 +0100

    [fish/go] Support ~/go as GOPATH

--- fish-abesto/conf.d/go.fish
+++ fish-abesto/conf.d/go.fish
@@ -1,2 +1,8 @@
 test -d ~/playground/go; and set -x GOPATH ~/playground/go
+test -d ~/go; and set -x GOPATH ~/go
+
+if test -n "$GOPATH"
+  set -x PATH $PATH $GOPATH/bin
+end
+
 test -d /usr/local/opt/go/libexec/bin; and set -x PATH $PATH /usr/local/opt/go/libexec/bin $GOPATH/bin
