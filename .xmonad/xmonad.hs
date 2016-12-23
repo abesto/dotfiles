03aed3d38e8ff326e0ea6a1bc3af3e9290243b26
Zoltan Nagy <abesto@abesto.net>
2016-12-23 10:40:20 +0100

    XMonad + setWMName hack for IntelliJ

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,12 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
 
 main :: IO ()
 main = xmonad =<< xmobar myConfig
 
+
+
 myConfig = def { terminal = "st"
+               , startupHook = setWMName "LG3D"
                }
