dd6ff2faf5e4f7803168d728049930f59d2b7777
Zoltan Nagy <abesto@abesto.net>
2017-01-01 16:03:33 +0100

    XMonad: unbind alt-enter (useful in IntelliJ)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,11 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
 
 main :: IO ()
-main = xmonad =<< xmobar myConfig
-
-
+main = xmonad =<< xmobar myConfig 
 
 myConfig = def { terminal = "st"
                , startupHook = setWMName "LG3D"
-               }
+               } `removeKeysP` ["M-<Return>"]
