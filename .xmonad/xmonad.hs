7a237626278ec6d8c64e4ff26753b09042593215
Zoltan Nagy <abesto@abesto.net>
2017-02-18 14:32:41 +0100

    urxvt colors. xmonad starts tmux in urxvt.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,6 +6,6 @@ import XMonad.Util.EZConfig
 main :: IO ()
 main = xmonad =<< xmobar myConfig
 
-myConfig = def { terminal = "urxvt"
+myConfig = def { terminal = "urxvt -e bash --norc -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$(hostname)\""
                , startupHook = setWMName "LG3D"
                } `removeKeysP` ["M-<Return>"]
