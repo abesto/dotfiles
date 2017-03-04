0eb7d683856c5a07b8f33a6bec6290ca2731e48f
Zoltan Nagy <abesto@abesto.net>
2017-03-04 18:45:10 +0100

    [bin] Clean up unused scripts. New script to start usual apps.

---
+++ bin/apps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+start_once() {
+    command="$1"; shift
+    if [ $# -gt 0 ]; then
+        pattern="$1"; shift
+    else
+        pattern="$command"
+    fi
+    if ! pgrep "$pattern" > /dev/null; then
+        $command &
+    fi
+}
+
+start_once emacs
+start_once spotify
+start_once wmail
+start_once google-chrome-stable chrome
+start_once slack
--- bin/sr_tee.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-set h $argv[1]
-set cmd $argv[2]
-ssh "$h" -l root "$cmd" | tee "$h.out"
