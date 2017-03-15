28754e23a0715829dc7e52bc07ca63fcb85b0872
Zoltan Nagy <abesto@abesto.net>
2017-03-15 21:53:14 +0100

    xmobar: more conscise. always show main and VPN nets.

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -6,23 +6,21 @@ Config {
     font = "xft:Source Code Pro:style=Semibold:size=8",
     lowerOnStart = True,
     commands = [
-        Run MultiCpu ["-t","Cpu: <total0>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
-        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","Swap: <used>/<total>","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run DynNetwork ["-t","<dev>: <rxvbar><rx>/<tx><txvbar>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run MultiCpu ["-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-p","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-p","3"] 10,
+        Run Network "wlp2s0" ["-H","1024","-L","100","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","4"] 10,
+        Run Network "tun0" ["-H","1024","-L","100","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","4"] 10,
         Run Date "%a %b %_d %Y %H:%M:%S" "date" 10,
         Run StdinReader,
-        Run TopProc [] 10,
-        Run TopMem [] 10,
-        Run Battery ["-t", "Battery: <acstatus> <timeleft> (<left>%)",
+        Run TopProc ["-w","15","-a","r"] 10,
+        Run Battery ["-t", "Bat: <acstatus> <timeleft> (<left>%)",
                "-L", "10", "-H", "80", "-p", "3",
                "--", "-O", "<fc=green>On</fc> - ", "-i", "",
                "-L", "-15", "-H", "-5",
                "-l", "red", "-m", "blue", "-h", "green"]
-              600,
-        Run DiskIO [("/", "Disk: <readvbar>R|W<writevbar>")] [] 10
+              600
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %top%   %multicpu%   %memory%   %diskio%   %swap%   %dynnetwork%   %battery%   <fc=#FFFFCC>%date%</fc>"
+    template = "%StdinReader%}{<%top%><%multicpu%><%memory%><%wlp2s0%><VPN=%tun0%><%battery%><fc=#FFFFCC>%date%</fc>"
 }
