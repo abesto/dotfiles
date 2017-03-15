9e923552ccd51794ce518b3e861724eb964614a8
Zoltan Nagy <abesto@abesto.net>
2017-03-15 22:42:50 +0100

    [xmobar] Add thermal zone values that have (non-passive) trip points

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -7,6 +7,8 @@ Config {
     lowerOnStart = True,
     commands = [
         Run MultiCpu ["-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-p","3"] 10,
+        Run ThermalZone 0 ["-H","80"] 10,  -- critical trip point is 107
+        Run ThermalZone 7 ["-H","70"] 10,  -- critical trip point is 100
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-p","3"] 10,
         Run Network "wlp2s0" ["-H","1024","-L","100","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","4"] 10,
         Run Network "tun0" ["-H","1024","-L","100","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","4"] 10,
@@ -22,5 +24,5 @@ Config {
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader%}{<%top%><%multicpu%><%memory%><%wlp2s0%><VPN=%tun0%> %battery% <fc=#FFFFCC>%date%</fc>"
+    template = "%StdinReader%}{<%top%><%multicpu% %thermal0% %thermal7%><%memory%><%wlp2s0%><VPN=%tun0%> %battery% <fc=#FFFFCC>%date%</fc>"
 }
