f0bb23b022fdd40189710fd3bfe30dd602c57480
Zoltan Nagy <abesto0@gmail.com>
2017-11-30 18:26:27 +0100

    [backup] More exclude rules, fix umount, chmod +x

--- bin/backup/create.sh
+++ bin/backup/create.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
-borg create -p -v --stats --exclude '/home/*/.cache' "${bak_mnt:-/mnt}"/borgbackup::'{hostname}-{now:%Y-%m-%d}' "$HOME"
+borg create -p -v --stats \
+	--exclude-caches \
+	--exclude "*/.Trash-*" \
+	--exclude "*/.Trash/*" \
+	--exclude "*/.[Cc]ache/*" \
+	--exclude "*/.[Cc]aches/*" \
+	--exclude "*/[Cc]ache/*" \
+	--exclude "*/[Cc]aches/*" \
+	--exclude "*/.IntelliJ*/index/*" \
+	--exclude "*/node_modules/*" \
+	--exclude "*/virtualenv/*" \
+	--exclude "$HOME/.m2/*" \
+	"${bak_mnt:-/mnt}"/borgbackup::'{hostname}-{now:%Y-%m-%d}' "$HOME"
