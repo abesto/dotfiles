ce52573efdc70ed87c81a95b3d7d056aa7a4c9b8
Zoltan Nagy <abesto0@gmail.com>
2018-03-10 09:34:31 +0100

    [backup] Reindent, add new exclude rule

--- bin/backup/create.sh
+++ bin/backup/create.sh
@@ -3,15 +3,16 @@
 set -euo pipefail
 
 borg create -p -v --stats \
-	--exclude-caches \
-	--exclude "*/.Trash-*" \
-	--exclude "*/.Trash/*" \
-	--exclude "*/.[Cc]ache/*" \
-	--exclude "*/.[Cc]aches/*" \
-	--exclude "*/[Cc]ache/*" \
-	--exclude "*/[Cc]aches/*" \
-	--exclude "*/.IntelliJ*/index/*" \
-	--exclude "*/node_modules/*" \
-	--exclude "*/virtualenv/*" \
-	--exclude "$HOME/.m2/*" \
-	"${bak_mnt:-/mnt}"/borgbackup::'{hostname}-{now:%Y-%m-%d}' "$HOME"
+     --exclude-caches \
+     --exclude "*/.Trash-*" \
+     --exclude "*/.Trash/*" \
+     --exclude "*/.[Cc]ache/*" \
+     --exclude "*/.[Cc]aches/*" \
+     --exclude "*/[Cc]ache/*" \
+     --exclude "*/[Cc]aches/*" \
+     --exclude "*/.IntelliJ*/index/*" \
+     --exclude "*/node_modules/*" \
+     --exclude "*/virtualenv/*" \
+     --exclude "$HOME/.m2/*" \
+     --exclude "*/backend/workdir/*" \
+     "${bak_mnt:-/mnt}"/borgbackup::'{hostname}-{now:%Y-%m-%d}' "$HOME"
