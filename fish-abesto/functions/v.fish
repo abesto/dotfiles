6793a24770f85efa71a586c05725591831c2c02f
Zoltan Nagy <abesto0@gmail.com>
2018-04-04 16:17:20 +0200

    Automatically activate virtualenv in PWD / ancestors of PWD

--- fish-abesto/functions/v.fish
+++ fish-abesto/functions/v.fish
@@ -1,8 +1,29 @@
 function v
-	for candidate in virtualenv venv ../virtualenv ../../virtualenv ../ ../../;
-		if [ -f $candidate/bin/activate.fish ];
+	for candidate in virtualenv venv ../virtualenv ../../virtualenv ../ ../../ ./;
+		if [ -f $candidate/bin/activate.fish ]
 			source $candidate/bin/activate.fish
 			return
 		end
 	end
+end
+
+function auto_v --on-variable PWD
+    set -l root "$PWD"
+    while [ "$root" != '/' ]
+      for candidate in virtualenv venv '';
+          set -l venv "$root/$candidate"
+          set -l activate "$venv/bin/activate.fish"
+          if [ -f "$activate" -a "$venv" != "$VIRTUAL_ENV" ];
+              if [ -n "$VIRTUAL_ENV" ]
+                  deactivate
+              end
+              source "$activate"
+              return
+          end
+      end
+      set -l root (dirname "$root")
+    end
+    if [ -n "$VIRTUAL_ENV" ]
+        deactivate
+    end
 end
