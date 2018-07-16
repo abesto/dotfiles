611b93a482a2775ae0479bf3eb6dc2789afb9aac
Zoltan Nagy <abesto0@gmail.com>
2018-07-16 19:39:02 +0200

    [backup] Add more excludes. Move hostname into repo name.

--- bin/backup/create.sh
+++ bin/backup/create.sh
@@ -14,5 +14,9 @@ borg create -p -v --stats \
      --exclude "*/node_modules/*" \
      --exclude "*/virtualenv/*" \
      --exclude "$HOME/.m2/*" \
+     --exclude "$HOME/.rvm/*" \
+     --exclude "$HOME/.gem/*" \
+     --exclude "$HOME/.sbt/*" \
+     --exclude "$HOME/.bundle/*" \
      --exclude "*/backend/workdir/*" \
-     "${bak_mnt:-/mnt}"/borgbackup::'{hostname}-{now:%Y-%m-%d}' "$HOME"
+     "${bak_mnt:-/mnt}"/'borgbackup-{hostname}::{now:%Y-%m-%d}' "$HOME"
